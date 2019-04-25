278e8d80850c327ef2186bc87879c09bd582a91c
Marcos Guedes <marcos.guedesjr@hotmail.com>
2019-04-25 15:21:20 -0300

    arquivos iniciais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { Push } from '@ionic-native/push/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -16,7 +18,8 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    Push
   ],
   bootstrap: [AppComponent]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Push, PushObject, PushOptions } from '@ionic-native/push/ngx';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,56 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  
+  constructor(
+    private push: Push,
+    private alertController: AlertController
+  ) { 
+    // to check if we have permission
+    this.push.hasPermission()
+      .then((res: any) => {
+        if (res.isEnabled) {
+          console.log('We have permission to send push notifications');
+          this.initPush();
+        } else {
+          console.log('We do not have permission to send push notifications');
+        }
+    });
+  }
 
+  async presentAlert(notification:any) {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: notification.message,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  initPush(){
+    let self = this;
+    const options: PushOptions = {
+      android: {},
+      ios: {
+          alert: 'true',
+          badge: true,
+          sound: 'false'
+      },
+      windows: {},
+      browser: {
+          pushServiceURL: 'http://push.api.phonegap.com/v1/push'
+      }
+   }
+
+   const pushObject: PushObject = this.push.init(options);
+
+   pushObject.on('notification').subscribe((notification: any) => {
+     console.log('Received a notification', notification)
+     self.presentAlert(notification);
+    });
+   pushObject.on('registration').subscribe((registration: any) => console.log('Device registered', registration));
+   pushObject.on('error').subscribe(error => console.error('Error with Push plugin', error));
+  }
 }
